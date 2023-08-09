3ebc00bbe9fd71359a942f98a792366e2a9303e5
kyusoo <rbtn110@uengine.org>
2023-08-09 17:09:06 +0900

    correlationGetSet 조건식 추가

--- src/main/java/com/eventstorming/domain/AggregateRoot.java
+++ src/main/java/com/eventstorming/domain/AggregateRoot.java
@@ -112,8 +112,10 @@ public class {{namePascalCase}} {{#checkExtends aggregateRoot.entities.relations
         
         {{#triggerByCommand}}
         {{eventValue.namePascalCase}} {{eventValue.nameCamelCase}} = new {{eventValue.namePascalCase}}(this);
-        {{#correlationGetSet .. eventValue}} 
+        {{#correlationGetSet .. eventValue}}
+        {{#if target}}
         {{../eventValue.nameCamelCase}}.set{{target.namePascalCase}}({{../../nameCamelCase}}Command.get{{source.namePascalCase}}());
+        {{/if}}
         {{/correlationGetSet}}
         {{eventValue.nameCamelCase}}.publishAfterCommit();
 
