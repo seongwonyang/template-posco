ef17633c8fba705eb38811aae2f210b74f1a2bf8
kyusoo <rbtn110@uengine.org>
2023-09-05 17:21:10 +0900

    init.sh 원복

--- for-model/init.sh
+++ for-model/init.sh
@@ -15,6 +15,8 @@ sudo mv /tmp/eksctl /usr/local/bin
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 . ~/.nvm/nvm.sh
+nvm install 14.19.0 && nvm use 14.19.0
+export NODE_OPTIONS=--openssl-legacy-provider
 
 cd infra
 docker-compose up
