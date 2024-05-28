b1aa7e0df0180ace93562e7db493b9bb851ea32d
kyusoo <rbtn110@uengine.org>
2024-05-28 16:07:40 +0900

    result, uri별 코드젠 수정

--- src/main/java/com/eventstorming/domain/Repository.java
+++ src/main/java/com/eventstorming/domain/Repository.java
@@ -23,7 +23,7 @@ public interface {{namePascalCase}}Repository extends PagingAndSortingRepository
     @Query(value = "select {{../nameCamelCase}} " +
         "from {{../namePascalCase}} {{../nameCamelCase}} " +
         "where{{#queryParameters}}{{#checkParameterType className nameCamelCase ../../nameCamelCase}}{{/checkParameterType}}{{^@last}} and {{/@last}}{{/queryParameters}}")
-       List<{{../namePascalCase}}> findBy{{namePascalCase}}       
+       {{#queryOption}}{{#if multipleResult}}List<{{../../namePascalCase}}>{{else}}{{../../namePascalCase}} {{/if}}{{#if apiPath}}{{apiPath}}{{else}}findBy{{../namePascalCase}}{{/if}}{{/queryOption}}
 ({{#queryParameters}}{{className}} {{nameCamelCase}}{{^@last}}, {{/@last}}{{/queryParameters}}, Pageable pageable);
 {{/if}}
 {{/attached}}
