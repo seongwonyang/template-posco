33fc917290392c28cc171bf8f51c298b838e194a
kyusoo <rbtn110@uengine.org>
2024-08-01 16:08:57 +0900

    external 수정

--- src/main/java/com/eventstorming/external/ExternalServiceQueryForAggregate.java
+++ src/main/java/com/eventstorming/external/ExternalServiceQueryForAggregate.java
@@ -23,15 +23,13 @@ import java.util.List;
  
 {{#target}}
 public interface {{aggregate.namePascalCase}}Service {
-    {{#queryOption}}
-    {{#if multipleResult}}
-    @GetMapping(path="/{{../aggregate.namePlural}}/search/{{#if useDefaultUri}}findBy{{apiPath}}{{else}}findBy{{../namePascalCase}}{{/if}}")
-    public List<{{../aggregate.namePascalCase}}> {{#if useDefaultUri}}{{apiPath}}{{else}}{{../nameCamelCase}}{{/if}}({{../namePascalCase}}Query {{../nameCamelCase}}query);
+    {{#if queryOption.multipleResult}}
+    @GetMapping(path="/{{aggregate.namePlural}}/search/{{#if queryOption.useDefaultUri}}findBy{{queryOption.apiPath}}{{else}}findBy{{namePascalCase}}{{/if}}")
+    public List<{{aggregate.namePascalCase}}> {{#if queryOption.useDefaultUri}}{{queryOption.apiPath}}{{else}}{{nameCamelCase}}{{/if}}({{namePascalCase}}Query {{nameCamelCase}}query);
     {{else}}
-    @GetMapping(path="/{{../aggregate.namePlural}}/search/{{#if apiPath}}{{apiPath}}{{else}}findBy{{../namePascalCase}}{{/if}}")
-    public {{../aggregate.namePascalCase}} {{#if useDefaultUri}}{{apiPath}}{{else}}{{../nameCamelCase}}{{/if}}(@PathVariable{{#../queryParameters}}{{#if isKey}}("{{nameCamelCase}}") {{className}} {{nameCamelCase}}, ({/if}){{/../queryParameters}}{{../namePascalCase}}Query {{../nameCamelCase}}query);
+    @GetMapping(path="/{{aggregate.namePlural}}/search/{{#if queryOption.apiPath}}findBy{{queryOption.apiPath}}{{else}}findBy{{namePascalCase}}{{/if}}")
+    public {{aggregate.namePascalCase}} {{#if queryOption.useDefaultUri}}{{queryOption.apiPath}}{{else}}{{nameCamelCase}}{{/if}}(@PathVariable{{#queryParameters}}{{#if isKey}}("{{nameCamelCase}}") {{className}} {{nameCamelCase}}, ({/if}){{/queryParameters}}{{namePascalCase}}Query {{nameCamelCase}}query);
     {{/if}}
-    {{/queryOption}}
 
 }
 
