28fb146176de36f405c4d0e09e659e058313cc3f
kyusoo <rbtn110@uengine.org>
2024-08-01 16:12:36 +0900

    external 수정

--- src/main/java/com/eventstorming/external/ExternalServiceQueryForAggregate.java
+++ src/main/java/com/eventstorming/external/ExternalServiceQueryForAggregate.java
@@ -25,10 +25,10 @@ import java.util.List;
 public interface {{aggregate.namePascalCase}}Service {
     {{#if queryOption.multipleResult}}
     @GetMapping(path="/{{aggregate.namePlural}}/search/{{#if queryOption.useDefaultUri}}findBy{{queryOption.apiPath}}{{else}}findBy{{namePascalCase}}{{/if}}")
-    public List<{{aggregate.namePascalCase}}> {{#if queryOption.useDefaultUri}}{{queryOption.apiPath}}{{else}}{{nameCamelCase}}{{/if}}({{namePascalCase}}Query {{nameCamelCase}}query);
+    public List<{{aggregate.namePascalCase}}> {{#if queryOption.useDefaultUri}}{{queryOption.apiPath}}{{else}}{{nameCamelCase}}{{/if}}({{namePascalCase}}Query {{nameCamelCase}}Query);
     {{else}}
     @GetMapping(path="/{{aggregate.namePlural}}/search/{{#if queryOption.apiPath}}findBy{{queryOption.apiPath}}{{else}}findBy{{namePascalCase}}{{/if}}")
-    public {{aggregate.namePascalCase}} {{#if queryOption.useDefaultUri}}{{queryOption.apiPath}}{{else}}{{nameCamelCase}}{{/if}}(@PathVariable{{#queryParameters}}{{#if isKey}}("{{nameCamelCase}}") {{className}} {{nameCamelCase}}, ({{/if}}){{/queryParameters}}{{namePascalCase}}Query {{nameCamelCase}}query);
+    public {{aggregate.namePascalCase}} {{#if queryOption.useDefaultUri}}{{queryOption.apiPath}}{{else}}{{nameCamelCase}}{{/if}}(@PathVariable{{#queryParameters}}{{#if isKey}}("{{nameCamelCase}}") {{className}} {{nameCamelCase}}, {{/if}}{{/queryParameters}}{{namePascalCase}}Query {{nameCamelCase}}Query);
     {{/if}}
 
 }
