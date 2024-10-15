0283385c3112bbb68a7cad8d146f00638d60b7be
kyusoo <rbtn110@uengine.org>
2024-10-15 10:50:51 +0900

    원복

--- src/main/java/com/eventstorming/external/ExternalQuery.java
+++ src/main/java/com/eventstorming/external/ExternalQuery.java
@@ -1,5 +1,5 @@
 forEach: Relation
-fileName: {{#target}}{{#if queryOption.apiPath}}{{#changeUpper queryOption.apiPath}}{{/changeUpper}}{{else}}{{namePascalCase}}{{/if}}{{/target}}Query.java
+fileName: {{pascalCase target.name}}Query.java
 path: {{source.boundedContext.name}}/{{options.packagePath}}/external
 except: {{contexts.except}}
 ---
@@ -29,9 +29,5 @@ this.contexts.except = !(
  
 if(!this.contexts.except){  
 }
-
-window.$HandleBars.registerHelper('changeUpper', function (name) {
-    return name.charAt(0).toUpperCase() + name.slice(1);
-});
  
 </function>
