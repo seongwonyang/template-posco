836a6a36479aef7a5f5d7865cea05195d36cbbe3
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-10-24 12:13:29 +0900

    a

--- domain/src/main/java/com/posco/domain/Repository.java
+++ domain/src/main/java/com/posco/domain/Repository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 {{/if}}
-import {{options.package}}.s20a01.domain.*;
+import com.posco.{{boundedContext.name}}.s20a01.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
