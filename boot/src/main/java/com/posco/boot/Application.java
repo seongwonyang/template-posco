4955b602848297164eede7700e5efa5dab94166e
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-10-28 16:14:22 +0900

    jpa 설정 수정

--- boot/src/main/java/com/posco/boot/Application.java
+++ boot/src/main/java/com/posco/boot/Application.java
@@ -5,6 +5,7 @@ package com.posco.{{name}}.s20a01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
