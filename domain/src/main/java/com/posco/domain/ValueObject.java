ca4bdff655dd725ec91cd7f05217df12d3df4e71
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-10-31 10:02:45 +0900

    a

--- domain/src/main/java/com/posco/domain/ValueObject.java
+++ domain/src/main/java/com/posco/domain/ValueObject.java
@@ -44,11 +44,13 @@ public class {{namePascalCase}} {{#checkCompositeKey incomingClassRef namePascal
 //>>> DDD / Value Object
 <function>
 window.$HandleBars.registerHelper('checkCompositeKey', function (incomingClassRef, voName) {
-    for(var i = 0; i < incomingClassRef.length; i ++ ){
-        if(incomingClassRef[i].isAggregateRoot){
-            for(var j = 0; j < incomingClassRef[i].fieldDescriptors.length; j ++ ){
-                if(incomingClassRef[i].fieldDescriptors[j] && incomingClassRef[i].fieldDescriptors[j].className===voName){
-                    return true;
+    if(incomingClassRef){
+        for(var i = 0; i < incomingClassRef.length; i ++ ){
+            if(incomingClassRef[i].isAggregateRoot){
+                for(var j = 0; j < incomingClassRef[i].fieldDescriptors.length; j ++ ){
+                    if(incomingClassRef[i].fieldDescriptors[j] && incomingClassRef[i].fieldDescriptors[j].className===voName){
+                        return true;
+                    }
                 }
             }
         }
