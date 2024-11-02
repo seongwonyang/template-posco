160785ae5591cba2c39fbd6de671e6cbd3b78c7c
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-02 17:38:13 +0900

    a

--- service/src/main/java/com/posco/service/Command.java
+++ service/src/main/java/com/posco/service/Command.java
@@ -9,9 +9,9 @@ import java.util.*;
 import lombok.Data;
 import java.time.LocalDate;
 {{#fieldDescriptors}}
-{{#isVO}}
+{{^isPrimitive className}}
 import com.posco.{{../boundedContext.name}}.s20a01.domain.{{className}};
-{{/isVO}}
+{{/isPrimitive}}
 {{/fieldDescriptors}}
 {{#checkBigDecimal fieldDescriptors}}{{/checkBigDecimal}}
 
