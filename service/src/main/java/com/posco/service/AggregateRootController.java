cd2038e8dd2670ab5a46fa2c3812ba936032e5c8
양성원 <seongwon@yangseong-won-ui-MacBookPro.local>
2024-11-05 11:11:06 +0900

    a

--- service/src/main/java/com/posco/service/AggregateRootController.java
+++ service/src/main/java/com/posco/service/AggregateRootController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
 import javax.validation.Valid;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 @RepositoryRestController
 public class {{namePascalCase}}Controller {
